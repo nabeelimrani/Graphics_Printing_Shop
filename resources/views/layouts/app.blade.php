b8861c86452c41d7201c5d0b56f09f4a121022ba
Nabeel Mehdi Imrani <nabeelemraane09@gmail.com>
2023-11-07 14:53:51 +0500

    update

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,5 +1,6 @@
 <!doctype html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
@@ -12,8 +13,8 @@
     <link rel="stylesheet" href="{{ asset('css/bootstrap.min.css') }}">
     <link rel="stylesheet" href="{{ asset('css/bootstrap.css') }}">
     <link rel="stylesheet" href="{{ asset('fontawesome/css/fontawesome.min.css') }}">
-  <link rel="stylesheet" href="{{ asset('fontawesome/css/all.min.css') }}">
-  <link rel="stylesheet" href="{{asset('frontend/dist/css/adminlte.min.css')}}">
+    <link rel="stylesheet" href="{{ asset('fontawesome/css/all.min.css') }}">
+    <link rel="stylesheet" href="{{ asset('frontend/dist/css/adminlte.min.css') }}">
 
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
     <style>
@@ -23,14 +24,16 @@
             background-repeat: no-repeat;
             background-attachment: fixed;
         }
+
         .navbar {
             background: rgba(255, 255, 255, 0.5);
         }
     </style>
 </head>
+
 <body>
     <div id="app">
-        
+
         <!-- <nav class="navbar navbar-expand-md navbar-light shadow-sm ">
             <div class="container">
                 <a class="navbar-brand" href="{{ url('/home') }}">
@@ -39,41 +42,41 @@
                 <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
                     <span class="navbar-toggler-icon"></span>
                 </button>
-                
+
                 <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                    
+
                     <ul class="navbar-nav me-auto"></ul>
                     <ul class="navbar-nav ms-auto">
                         @guest
-                            @if (Route::has('login'))
-                                <li class="nav-item">
-                                    <a class="nav-link btn btn-info rounded-pill btn-primary p-2" href="{{ route('login') }}">
-                                        <i class="fa fa-sign-in"></i>&nbsp;Login
-                                    </a>
-                                </li>
-                            @endif
-                            &nbsp;&nbsp;&nbsp;
-                            @if (Route::has('register'))
-                                <li class="nav-item">
-                                    <a class="nav-link btn btn-info rounded-pill btn-primary p-2" href="{{ route('register') }}">
-                                        <i class="fa fa-user-plus"></i>&nbsp;Register
-                                    </a>
-                                </li>
-                            @endif
-                        @else
-                            <li class="nav-item dropdown">
-                                <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
-                                    {{ Auth::user()->name }}
-                                </a>
-                                <div class="dropdown-menu dropdown-menu-end" aria-labelledby="navbarDropdown">
-                                    <a class="dropdown-item" href="{{ route('logout') }}" onclick="event.preventDefault(); document.getElementById('logout-form').submit();">
-                                        {{ __('Logout') }}
-                                    </a>
-                                    <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
-                                        @csrf
-                                    </form>
-                                </div>
-                            </li>
+                                    @if (Route::has('login'))
+    <li class="nav-item">
+                                            <a class="nav-link btn btn-info rounded-pill btn-primary p-2" href="{{ route('login') }}">
+                                                <i class="fa fa-sign-in"></i>&nbsp;Login
+                                            </a>
+                                        </li>
+    @endif
+                                    &nbsp;&nbsp;&nbsp;
+                                    @if (Route::has('register'))
+    <li class="nav-item">
+                                            <a class="nav-link btn btn-info rounded-pill btn-primary p-2" href="{{ route('register') }}">
+                                                <i class="fa fa-user-plus"></i>&nbsp;Register
+                                            </a>
+                                        </li>
+    @endif
+@else
+    <li class="nav-item dropdown">
+                                        <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
+                                            {{ Auth::user()->name }}
+                                        </a>
+                                        <div class="dropdown-menu dropdown-menu-end" aria-labelledby="navbarDropdown">
+                                            <a class="dropdown-item" href="{{ route('logout') }}" onclick="event.preventDefault(); document.getElementById('logout-form').submit();">
+                                                {{ __('Logout') }}
+                                            </a>
+                                            <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
+                                                @csrf
+                                            </form>
+                                        </div>
+                                    </li>
                         @endguest
                     </ul>
                 </div>
@@ -84,7 +87,8 @@
         </main>
     </div>
     <script src="{{ asset('js/bootstrap.bundle.js') }}"></script>
-    <script src="{{asset('fontawesome/js/fontawesome.min.js')}}"></script>
-<script src="{{asset('fontawesome/js/all.min.js')}}"></script>
+    <script src="{{ asset('fontawesome/js/fontawesome.min.js') }}"></script>
+    <script src="{{ asset('fontawesome/js/all.min.js') }}"></script>
 </body>
+
 </html>
